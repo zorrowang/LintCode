8c1823b9bf7d837b3b31e0e9a5dcd34e7e423919
Hongwwei Wang <zorrowanghw@gmail.com>
2019-12-30 22:30:36 -0500

    add 911

---
+++ 911.java
@@ -0,0 +1,21 @@
+public class Solution {
+  /**
+   * @param nums: an array
+   * @param k: a target value
+   * @return: the maximum length of a subarray that sums to k
+   */
+  public int maxSubArrayLen(int[] nums, int k) {
+      Map<Integer, Integer> map = new HashMap<>();
+      map.put(0, -1);
+      int maxLen = 0;
+      for (int sum = 0, i = 0; i < nums.length; i++) {
+          sum += nums[i];
+          if (map.containsKey(sum - k)) {
+              maxLen = Math.max(maxLen, i - map.get(sum - k));
+          } else {
+              map.putIfAbsent(sum, i);
+          }
+      }
+      return maxLen;
+  }
+}
